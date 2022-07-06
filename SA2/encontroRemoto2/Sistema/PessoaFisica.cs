6a36af1ae9049e3f5f95a84b31ca10adb320afee
welingtonvb <98372117+welingtonvb@users.noreply.github.com>
2022-07-05 22:04:54 -0300

    Alteracao do repositorio

---
+++ SA2/encontroRemoto2/Sistema/PessoaFisica.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema
+{
+    public class PessoaFisica : Pessoa
+    {
+        public string CPF { get; set; }       
+    }
+}
---
+++ SA2/encontroRemoto2/Sistema/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Sistema {
+  public class program {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello!");
+
+      Pessoa fulano = new Pessoa();
+      fulano.nome = "Fulano";
+
+      Console.WriteLine(fulano.nome);
+    }
+  }
+}
